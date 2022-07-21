a02af41cba567224727681f88f0399cfbd7c220e
Apollo3zehn <git@m1.apollo3zehn.net>
2022-07-21 17:54:27 +0200

    Tests.

--- build/print_solution.py
+++ build/print_solution.py
@@ -5,12 +5,12 @@ with open("solution.json", "r") as fh:
         
 print(
 f"""
-AUTHORS={solution_data["authors"]}
-COMPANY={solution_data["company"]}
-COPYRIGHT={solution_data["copyright"]}
-PRODUCT={solution_data["product"]}
-LICENSE={solution_data["license"]}
-PROJECT_URL={solution_data["project-url"]}
-REPOSITORY_URL={solution_data["repository-url"]}
-ICON_URL={solution_data["icon-url"]}
+AUTHORS="{solution_data["authors"]}"\n
+COMPANY="{solution_data["company"]}"\n
+COPYRIGHT="{solution_data["copyright"]}"\n
+PRODUCT="{solution_data["product"]}"\n
+LICENSE="{solution_data["license"]}"\n
+PROJECT_URL="{solution_data["project-url"]}"\n
+REPOSITORY_URL="{solution_data["repository-url"]}"\n
+ICON_URL="{solution_data["icon-url"]}"\n
 """)
