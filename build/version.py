e3e2634e55b95fc967c091737723534812561347
Apollo3zehn <git@m1.apollo3zehn.net>
2024-02-20 08:40:17 +0100

    Switch to .NET 8 and update build scripts

--- build/version.py
+++ build/version.py
@@ -2,11 +2,11 @@ import json
 from typing import Optional
 
 
-def get_version(build: Optional[str], as_pypi_version: bool = False) -> str:
+def get_version(build: Optional[str], version_type: str = "default") -> str:
 
     with open("version.json", "r") as fh:
         version_data = json.load(fh)
-        
+
     # version
     version = version_data["version"]
     suffix = version_data["suffix"]
@@ -17,7 +17,7 @@ def get_version(build: Optional[str], as_pypi_version: bool = False) -> str:
         if build:
 
             # PEP440 does not support SemVer versioning (https://semver.org/#spec-item-9)
-            if as_pypi_version:
+            if version_type == "pypi":
                 version = f"{version}{int(build):03d}"
 
             else:
